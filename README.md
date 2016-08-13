cba1ee1d91ec8c2551262638bf7791a170c5b78c
bjellesma <william.jellesma@gmail.com>
2016-08-13 15:57:38 -0400

    Update Readme and change dice images

---
+++ README.md
@@ -0,0 +1,23 @@
+# RubYahtzee
+## Table Of Contents
+* [What Is This](#What-is-this "What is this")
+* [Prerequisites](#Prerequisites "Prerequisites")
+* [Quick Start](#Quick-Start "Quick Start")
+* [Author](#Author "Author")
+* [Screenshots](#Screenshot "Screenshots")
+* [Licensing](#Licensing "Licensing")
+<a name="What-is-this"><h1>What Is This</h1></a>
+  <p>A game of yahtzee written in Ruby using the shoes library</p>
+<a name="Prerequisites"><h1>Prerequisites</h1></a>
+* Download and install [Shoes](http://shoesrb.com/downloads/ "Download Shoes") (v3.3 walkabout works best)
+<a name="Quick-Start"><h1>Quick Start</h1></a>
+* Clone this repo
+* Navigate to the directory containing the project and type <code>python PyCrypto.py</code>
+* `shoes dice.rb`
+<a name="Author"><h1>Author</h1></a>
+  <p>This project was written for fun and to learn ruby and the shoes library.
+  This project was authored by William Jellesma. </p>
+<a name="Screenshot"><h1>Screenshot</h1></a>
+![Screenshot](sample.PNG)
+<a name="Licensing"><h1>Licensing</h1></a>
+<p>This Project is licensed under the <a href="http://choosealicense.com/licenses/mit/">MIT License</a></p>
--- dice.rb
+++ dice.rb
@@ -8,12 +8,12 @@ Shoes.app :title => "dice" do
 	@img6 = nil
 	flow() do
 		style(:margin_left => '20%', :margin_top => '20%')
-		@img1 = image "one.gif"
-		@img2 = image "two.gif"
-		@img3 = image "three.gif"
-		@img4 = image "four.gif"
-		@img5 = image "five.gif"
-		@img6 = image "six.gif"
+		@img1 = image "one.png"
+		@img2 = image "two.png"
+		@img3 = image "three.png"
+		@img4 = image "four.png"
+		@img5 = image "five.png"
+		@img6 = image "six.png"
 	end
 
 	flow() do
@@ -28,95 +28,95 @@ Shoes.app :title => "dice" do
 
 			case num1
 				when 1
-					@img1.path = "one.gif"
+					@img1.path = "one.png"
 				when 2
-					@img1.path = "two.gif"
+					@img1.path = "two.png"
 				when 3
-					@img1.path = "three.gif"
+					@img1.path = "three.png"
 				when 4
-					@img1.path = "four.gif"
+					@img1.path = "four.png"
 				when 5
-					@img1.path = "five.gif"
+					@img1.path = "five.png"
 				when 6
-					@img1.path = "six.gif"
+					@img1.path = "six.png"
 			end
 
 			case num2
 				when 1
-					@img2.path = "one.gif"
+					@img2.path = "one.png"
 				when 2
-					@img2.path = "two.gif"
+					@img2.path = "two.png"
 				when 3
-					@img2.path = "three.gif"
+					@img2.path = "three.png"
 				when 4
-					@img2.path = "four.gif"
+					@img2.path = "four.png"
 				when 5
-					@img2.path = "five.gif"
+					@img2.path = "five.png"
 				when 6
-					@img2.path = "six.gif"
+					@img2.path = "six.png"
 			end
 
 			case num3
 				when 1
-					@img3.path = "one.gif"
+					@img3.path = "one.png"
 				when 2
-					@img3.path = "two.gif"
+					@img3.path = "two.png"
 				when 3
-					@img3.path = "three.gif"
+					@img3.path = "three.png"
 				when 4
-					@img4.path = "four.gif"
+					@img4.path = "four.png"
 				when 5
-					@img5.path = "five.gif"
+					@img5.path = "five.png"
 				when 6
-					@img6.path = "six.gif"
+					@img6.path = "six.png"
 			end
 
 			case num4
 				when 1
-					@img4.path = "one.gif"
+					@img4.path = "one.png"
 				when 2
-					@img4.path = "two.gif"
+					@img4.path = "two.png"
 				when 3
-					@img4.path = "three.gif"
+					@img4.path = "three.png"
 				when 4
-					@img4.path = "four.gif"
+					@img4.path = "four.png"
 				when 5
-					@img4.path = "five.gif"
+					@img4.path = "five.png"
 				when 6
-					@img4.path = "six.gif"
+					@img4.path = "six.png"
 			end
 
 			case num5
 				when 1
-					@img5.path = "one.gif"
+					@img5.path = "one.png"
 				when 2
-					@img5.path = "two.gif"
+					@img5.path = "two.png"
 				when 3
-					@img5.path = "three.gif"
+					@img5.path = "three.png"
 				when 4
-					@img5.path = "four.gif"
+					@img5.path = "four.png"
 				when 5
-					@img5.path = "five.gif"
+					@img5.path = "five.png"
 				when 6
-					@img5.path = "six.gif"
+					@img5.path = "six.png"
 			end
 
 			case num6
 				when 1
-					@img6.path = "one.gif"
+					@img6.path = "one.png"
 				when 2
-					@img6.path = "two.gif"
+					@img6.path = "two.png"
 				when 3
-					@img6.path = "three.gif"
+					@img6.path = "three.png"
 				when 4
-					@img6.path = "four.gif"
+					@img6.path = "four.png"
 				when 5
-					@img6.path = "five.gif"
+					@img6.path = "five.png"
 				when 6
-					@img6.path = "six.gif"
+					@img6.path = "six.png"
 			end
 
-			
+
 		end
 	end
 end
